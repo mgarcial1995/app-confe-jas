45f91c8fb70fdb53e82ccc29ec02794f01ef05fd
Martin <martingarcial-18@hotmail.com>
2025-07-25 09:57:34 -0500

    feat: descarga excel corregido

--- src/pages/Participantes.jsx
+++ src/pages/Participantes.jsx
@@ -31,9 +31,13 @@ const Participantes = () => {
 
   const descargarExcel = async () => {
     try {
-      const response = await exportarParticipantesAExcel()
+      const response = await exportarParticipantesAExcel(); // retorna blob ahora
 
-      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const blob = new Blob([response.data], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      });
+
+      const url = window.URL.createObjectURL(blob);
       const a = document.createElement('a');
       a.href = url;
       a.download = 'participantes.xlsx';
@@ -45,7 +49,6 @@ const Participantes = () => {
       console.error('Error al descargar el archivo:', error);
     }
   };
-
   return (
     <div>
       <Header text="Participantes" />
--- src/services/services.js
+++ src/services/services.js
@@ -1,16 +1,19 @@
 import api from "./apiClient";
 
-export const get = (path, params = {}) => api.get(path, { params });
+export const get = (path, options = {}) => api.get(path, options);
 export const post = (path, data) => api.post(path, data);
 export const put = (path, data) => api.put(path, data);
 export const del = (path) => api.delete(path);
 
-// También puedes hacer funciones específicas:
 export const crearParticipante = (data) =>
   post("/participante/crearParticipante", data);
 export const getParticipantes = () => get("/participante/obtenerParticipantes");
-export const exportarParticipantesAExcel = () => get("/participante/exportarParticipantesAExcel");
-
+// export const exportarParticipantesAExcel = () => get("/participante/exportarParticipantesAExcel");
+export const exportarParticipantesAExcel = async () => {
+  return get('/participante/exportarParticipantesAExcel', {
+    responseType: 'blob', // ✅ Esto ahora sí funciona como debería
+  });
+};
 export const asignarParticipanteACompania = (data) =>
   post("/participante/asignarParticipanteACompania", data);
 
