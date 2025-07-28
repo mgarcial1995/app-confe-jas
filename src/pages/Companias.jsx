bb8deaad6e300bce2ba4f23c0f5fb2d6face10f2
Martin <martingarcial-18@hotmail.com>
2025-07-28 10:58:22 -0500

    feat cambios

--- src/pages/Companias.jsx
+++ src/pages/Companias.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
-import { getCompanias } from "../services/services";
+import { getCompanias, getCompaniasConParticipantes } from "../services/services";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -10,7 +10,8 @@ const Companias = () => {
     obtenerCompanias();
   }, []);
   const obtenerCompanias = async () => {
-    const { data } = await getCompanias();
+    const { data } = await getCompaniasConParticipantes();
+    console.log(data)
     setCompanias(data);
   };
   return (
@@ -30,13 +31,31 @@ const Companias = () => {
             <Link
               to={"/compania/edit/" + comp.id}
               key={i}
-              className="w-28 h-28 bg-[#F8AE1A] text-3xl text-[#333333] hover:text-white font-bold rounded-xl flex gap-1 justify-center items-center"
+              className="w-28 h-28 bg-[#F8AE1A] text-3xl text-[#333333] hover:text-white font-bold rounded-xl flex flex-col gap-1 justify-center items-center"
             >
-              {comp.numero_compania}
-              <FontAwesomeIcon
-                className="text-3xl mt-2"
-                icon="flag"
-              />
+              <div className="flex gap-2 items-center">
+                {comp.numero_compania}
+                <FontAwesomeIcon
+                  className="text-3xl mt-2"
+                  icon="flag"
+                />
+              </div>
+              <div className="text-md flex gap-2">
+                <div className="flex items-center">
+                  <FontAwesomeIcon
+                    className="text-sm text-blue-500"
+                    icon="user"
+                  />
+                  <p className="text-lg">{comp.participantes.filter(e => e.sexo === "Masculino").length} </p>
+                </div>
+                <div className="flex items-center">
+                  <FontAwesomeIcon
+                    className="text-sm text-pink-600"
+                    icon="user"
+                  />
+                  <p  className="text-lg">{comp.participantes.filter(e => e.sexo === "Femenino").length} </p>
+                </div>
+              </div>
             </Link>
           );
         })}
