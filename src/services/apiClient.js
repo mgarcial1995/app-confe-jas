d38dbe303c9eeed5c9b325e8a9ffe197c8679079
Martin <martingarcial-18@hotmail.com>
2025-07-28 08:07:08 -0500

    feat: cambios

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3458/api/v1/",
-  // baseURL: "https://api-confe-jas-production.up.railway.app/api/v1/",
+  // baseURL: "http://localhost:3458/api/v1/",
+  baseURL: "https://api-confe-jas-production.up.railway.app/api/v1/",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
